55be2e03aafa4357f0ee9df739447da8328ad9ab
HowDoIprintHelloWorld <howdoiprintgit.ql4lv@simplelogin.com>
2024-03-06 21:35:03 +0100

    Minor changes

--- src/parser.go
+++ src/parser.go
@@ -1,48 +1,52 @@
 package src
 
 import (
-  "os"
-  "fmt"
-  "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func Make_tokens(script string) []Line {
-  var lines []Line
-  var isString, isLineComment, isFileComment bool
-  var stringChar rune;
-  var isEscaped int;
-  for line_number, line := range strings.Split(script, "\n") {
-    var lastCharacters string
-    var tokens []Token;
-    for _, character := range line {
-      switch character {
-        case '\\':
-          if isEscaped == 1 {
-            lastCharacters += "\\"
-          } else {
-            isEscaped = 2
-          }
-        case '"', '\'':
-          if isString {
-            if character == stringChar {isString = false
-            } else {lastCharacters += string(stringChar)}
-          } else {
-            isString = true
-            stringChar = character
-          }
-        case 
-      }
-    }
-    fmt.Println(line_number, line)
-  }
-  return lines
+	var lines []Line
+	var isString, isLineComment, isFileComment bool
+	var stringChar rune
+	var isEscaped int
+	operands := []string{"!=", "**", ">=", "=="}
+	for line_number, line := range strings.Split(script, "\n") {
+		var lastCharacters string
+		var tokens []Token
+		for _, character := range line {
+			switch character {
+			case '\\':
+				if isEscaped == 1 {
+					lastCharacters += "\\"
+				} else {
+					isEscaped = 2
+				}
+			case '"', '\'':
+				if isString {
+					if character == stringChar {
+						isString = false
+					} else {
+						lastCharacters += string(stringChar)
+					}
+				} else {
+					isString = true
+					stringChar = character
+				}
+			default:
+				lastCharacters += string(character)
+			}
+		}
+		fmt.Println(line_number, line)
+	}
+	return lines
 }
 
-
 func Read_script(file_name string) string {
-  file_content, err := os.ReadFile(file_name)
-  if err != nil {
-    panic(fmt.Sprintf("File not found: ", err))
-  }
-  return string(file_content)
+	file_content, err := os.ReadFile(file_name)
+	if err != nil {
+		panic(fmt.Sprintf("File not found: ", err))
+	}
+	return string(file_content)
 }
