f38f19844b2ebbddc08a860831e5826fa6aa2e32
HowDoIprintHelloWorld <87938212+HowDoIprintHelloWorld@users.noreply.github.com>
2024-03-07 10:46:06 +0000

    Started work on adding name characters

--- src/parser.go
+++ src/parser.go
@@ -6,15 +6,28 @@ import (
 	"strings"
 )
 
+func get_name_characters() []int {
+  var name_characters []int
+  // 48-58, 65-91, 97-122, 95
+  for i := 0; i < 122; i++ {
+    
+    name_characters
+  } 
+  return name_characters
+}
+
+
 func Make_tokens(script string) []Line {
 	var lines []Line
 	var isString, isLineComment, isFileComment bool
+  fmt.Println(isLineComment, isFileComment)
 	var stringChar rune
 	var isEscaped int
-	operands := []string{"!=", "**", ">=", "=="}
+  var name_characters = get_name_characters()
+	// operands := []string{"!=", "**", ">=", "=="}
 	for line_number, line := range strings.Split(script, "\n") {
 		var lastCharacters string
-		var tokens []Token
+		//var tokens []Token
 		for _, character := range line {
 			switch character {
 			case '\\':
@@ -35,7 +48,10 @@ func Make_tokens(script string) []Line {
 					stringChar = character
 				}
 			default:
+        if int
+        fmt.Println(int(character))
 				lastCharacters += string(character)
+        
 			}
 		}
 		fmt.Println(line_number, line)
